ba14a1f4987998520fd952daaaf15c419dfcfa0a
srinivasarao93900 <160144874+srinivasarao93900@users.noreply.github.com>
2025-02-20 11:38:56 +0530

    Add files via upload

---
+++ Assignments/DAY 4/Data_types.java
@@ -0,0 +1,14 @@
+class Data_types
+{
+	public static void main(String args[])
+	{
+		byte var1=10;  //it can store the 1byte of data
+		short var2=10; //it can store the 2bytes of data
+		int var3=10; //it can store the 4bytes of data
+		long var4=10;//it can store the 8bytes of data
+		float var5=10.66f;//it can store the 4bytes of data
+		double var6=10.534243;//it can store the 8bytes of data
+		boolean var7=true;//it can store the true/false
+		char var8='A';//it can store 2bytes of data
+	}
+}
---
+++ Assignments/DAY 4/Sum.java
@@ -0,0 +1,10 @@
+class Sum
+{
+	public static void main(String args[])
+	{
+		int a=5;
+		int b=6;
+		int c=a+b;
+		System.out.println("The sum of given two numbers "+a+" and "+b+" = "+c);
+	}
+}
