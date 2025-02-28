23a02295a704b95660e6e2862ac226d80d293822
srinivasarao93900 <160144874+srinivasarao93900@users.noreply.github.com>
2025-02-28 15:45:53 +0530

    Add files via upload

---
+++ Assignments/OOPS CONCEPTS/ScopeVar2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class ScopeVar2
+{
+	String StuName;//instance variables
+	String StuBranch;
+	float cgpa;
+	void display()  
+	{
+		Scanner sc=new Scanner(System.in);
+		 System.out.println("Enter the name of the student");
+		 StuName=sc.next();
+		 System.out.println("Enter the branch of the student");
+		 StuBranch=sc.next();
+		 System.out.println("Enter the cgpa of the student");
+		 cgpa=sc.nextFloat();
+		System.out.println("the name of the student is "+StuName);
+		System.out.println("the branch of the student is  "+StuBranch);
+	    System.out.println("the cgpa of the student is "+cgpa);
+		sc.close();
+	}
+	public static void main(String args[])
+	{
+		 ScopeVar2 obj=new ScopeVar2();
+		 obj.display();
+}}
